e9fb863482f78e01f38c3b3ddb009f24a83857ab
GharretWilcut <89796751+GharretWilcut@users.noreply.github.com>
2025-12-09 09:10:51 -0600

    Add tests for subtraction, multiplication, and division

--- test/test_app.py
+++ test/test_app.py
@@ -1,5 +1,5 @@
 
-from src.app import add 
+from src.app import add, sub,mult,div,log,sqrt,sin,cos,sqr,percent
 
 def test_add_1():
     assert add(5,6) == 11
@@ -9,3 +9,16 @@ def test_add_2():
 
 def test_add_3():
     assert add(5,6) != 12
+
+def test_subtract():
+    assert sub(5, 3) == 2
+
+def test_multiply():
+    assert mult(4, 3) == 12
+
+def test_divide_normal():
+    assert div(10, 2) == 5
+
+def test_divide_by_zero():
+    with pytest.raises(ZeroDivisionError):
+        div(5, 0)
